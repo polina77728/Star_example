75fcb9b7812cd1769becbe56feb07b30cf1da49a
Polina Balova <polina77728@yandex.ru>
2022-11-21 21:07:37 +0300

    Задание 2 со звездочкой.

---
+++ 2_Star/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+*/
+
+
+string message = "ПОРА ПООБЕДАТЬ! УРА!";
+string parol = "123456789";
+string exit = "exit";
+
+
+Console.WriteLine("Привет! Тебе пришло секретное сообщение!");
+Console.WriteLine("У тебя есть 3 попытки ввести пароль.");
+Console.WriteLine("ВВЕДИТЕ ПАРОЛЬ: ");
+string parolUser = Console.ReadLine();
+
+
+while (parolUser != parol || parolUser == parol)
+{
+
+    if (parolUser == parol)
+    {
+        Console.WriteLine(message);
+        break;
+    }
+
+    else if (parolUser == exit)
+    {
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("ВВЕДИТЕ ПАРОЛЬ: ");
+        parolUser = Console.ReadLine();
+    }
+}
